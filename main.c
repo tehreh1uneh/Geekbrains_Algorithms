c05bdcce00a9b244642e7d3770feeef58e328910
tehreh1uneh <andrey.solovetov@gmail.com>
2018-03-10 15:06:07 +1000

    lesson 4

--- main.c
+++ main.c
@@ -7,67 +7,71 @@
 */
 #include <stdio.h>
 
-#define X 6
+#define X 5
 #define Y 8
+int board[Y][X];
 
 struct point {
     int x, y;
 };
 typedef struct point Point;
-int board[X][Y];
 
 void printBoard() {
-    for (int i = 0; i < Y; i++) {
-        for (int j = 0; j < X; j++)
-            printf("%4d", board[j][i]);
+    for (int y = 0; y < Y; y++) {
+        for (int x = 0; x < X; x++) {
+            printf("%4d", board[y][x]);
+        }
         printf("\n");
     }
 }
 
 void annull() {
-    for (int i = 0; i < Y; i++)
-        for (int j = 0; j < X; j++)
-            board[j][i] = 0;
+    for (int y = 0; y < Y; y++)
+        for (int x = 0; x < X; x++)
+            board[y][x] = 0;
 }
 
-int fillAndReturn(int x, int y, int res) {
-    board[x][y] = res;
+int fillAndReturn(int y, int x, int res) {
+    board[y][x] = res;
     return res;
 }
 
-void addBarrier(Point *barriers, int index, int x, int y) {
-    barriers[index].x = x;
-    barriers[index].y = y;
-}
-
-int waysNumber(int x, int y, Point barriers[], int barriersNumber) {
-    for (int i = 0; i < barriersNumber; ++i) {
-        if (barriers[i].x == x && barriers[i].y == y) {
-            return fillAndReturn(x, y, 0);
+int waysNumber(int y, int x, Point *barriers, int barriersNumber) {
+    for (int i = 0; i < barriersNumber; i++) {
+        if (barriers[i].y == y && barriers[i].x == x) {
+            return fillAndReturn(y, x, 0);
         }
     }
 
     if (x == 0 && y == 0) {
-        return fillAndReturn(x, y, 1);
+        return fillAndReturn(y, x, 1);
     } else if (x == 0) {
-        return fillAndReturn(x, y, waysNumber(x, y - 1, barriers, barriersNumber));
+        return fillAndReturn(y, x, waysNumber(y - 1, x, barriers, barriersNumber));
     } else if (y == 0) {
-        return fillAndReturn(x, y, waysNumber(x - 1, y, barriers, barriersNumber));
+        return fillAndReturn(y, x, waysNumber(y, x - 1, barriers, barriersNumber));
     } else {
-        return fillAndReturn(x, y, waysNumber(x - 1, y, barriers, barriersNumber) +
-                                   waysNumber(x, y - 1, barriers, barriersNumber));
+        return fillAndReturn(y, x, waysNumber(y - 1, x, barriers, barriersNumber) +
+                                   waysNumber(y, x - 1, barriers, barriersNumber));
     }
 }
 
+void addBarrier(Point* barriers, int index, int y, int x){
+    barriers[index].y = y;
+    barriers[index].x = x;
+}
+
 int main(void) {
+    int lastXIndex = X - 1;
+    int lastYIndex = Y - 1;
+
     /*
      * Сначала проверяем корректно ли сработает без барьеров
      */
     {
         Point barriers[0];
         annull();
-        waysNumber(X, Y, barriers, 0);
-        printf("Without barriers:\n");
+        int res = waysNumber(lastYIndex, lastXIndex, barriers, 0);
+        printf("Without barriers: %d\n", res);
         printBoard();
         printf("\n");
     }
@@ -79,8 +83,8 @@ int main(void) {
         Point barriers[1];
         addBarrier(barriers, 0, 0, 0);
         annull();
-        waysNumber(X, Y, barriers, 1);
-        printf("Barrier in {1,1}:\n");
+        int res = waysNumber(lastYIndex, lastXIndex, barriers, 1);
+        printf("Barrier in {1,1}: %d\n", res);
         printBoard();
         printf("\n");
     }
@@ -90,10 +94,10 @@ int main(void) {
      */
     {
         Point barriers[1];
-        addBarrier(barriers, 0, 2, 2);
+        addBarrier(barriers, 0, 3, 2);
         annull();
-        printf("Barrier in {3,3}:\n");
-        waysNumber(X, Y, barriers, 1);
+        int res = waysNumber(lastYIndex, lastXIndex, barriers, 1);
+        printf("Barrier in {3,4}: %d\n", res);
         printBoard();
         printf("\n");
     }
@@ -103,10 +107,10 @@ int main(void) {
     */
     {
         Point barriers[1];
-        addBarrier(barriers, 0, X - 1, Y - 1);
+        addBarrier(barriers, 0, lastYIndex, lastXIndex);
         annull();
-        waysNumber(X, Y, barriers, 1);
-        printf("Barrier in {%d,%d}:\n", X, Y);
+        int res = waysNumber(lastYIndex, lastXIndex, barriers, 1);
+        printf("Barrier in {%d,%d}: %d\n", X, Y, res);
         printBoard();
         printf("\n");
     }
@@ -116,12 +120,12 @@ int main(void) {
      */
     {
         Point barriers[3];
-        addBarrier(barriers, 0, 0, 3);
-        addBarrier(barriers, 1, 1, 3);
-        addBarrier(barriers, 2, 4, 2);
+        addBarrier(barriers, 0, 3, 0);
+        addBarrier(barriers, 1, 3, 1);
+        addBarrier(barriers, 2, 2, 4);
         annull();
-        waysNumber(X, Y, barriers, 3);
-        printf("Barriers in {1,4}, {2,4}, {5,3}:\n");
+        int res = waysNumber(lastYIndex, lastXIndex, barriers, 3);
+        printf("Barriers in {1,4} {2,4} {5,3}: %d\n", res);
         printBoard();
         printf("\n");
     }
