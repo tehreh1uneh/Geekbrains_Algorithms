376e330c7661e5293627ac49eff08661e802e939
tehreh1uneh <andrey.solovetov@gmail.com>
2018-03-22 22:06:34 +1000

    lesson 7

--- main.c
+++ main.c
@@ -8,92 +8,156 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define T int
 
-typedef struct Node {
+typedef struct Stack {
+    int size;
+    struct GraphNode *head;
+} Stack;
+
+typedef struct GraphNode {
     T data;
-/*
- * На занятии прописали parent, но он нигде не используется, а нужен ли вообще?
- * Логично было бы заполнять, но в примерах в методичке этого нет.
- */
-//    struct Node *parent;
-    struct Node *left;
-    struct Node *right;
-} Node;
+    int index;
+    bool used;
+    struct Stack *children;
 
-// На занятии здесь было FILE file, странно, что компилятор не подсветил
-FILE *file;
+    struct GraphNode *next;
+    struct GraphNode *prev;
+} GraphNode;
 
-int simpleHash(const char *string, int length) {
-    int result = 0;
-    for (int i = 0; i < length; ++i) {
-        result += string[i];
-    }
-    return result;
+typedef struct Queue {
+    GraphNode *head;
+    GraphNode *tail;
+    int size;
+} Queue;
+
+void pushQ(Queue *q, GraphNode *value) {
+    GraphNode *tmp = (GraphNode *) malloc(sizeof(GraphNode));
+    if (tmp == NULL) return;
+
+    tmp->data = value->data;
+    tmp->index = q->size++;
+    tmp->prev = q->tail;
+    tmp->next = NULL;
+
+    if (q->tail != NULL)
+        q->tail->next = tmp;
+    else
+        q->head = tmp;
+
+    q->tail = tmp;
 }
 
-// Копипаст с лекции
-Node *tree(int n) {
-    Node *newNode;
-    int nL;
-    int nR;
-    if (n == 0)
-        newNode = NULL;
-    else {
-        int value;
-        fscanf(file, "%d", &value);
-        nL = n / 2;
-        nR = n - nL - 1;
-        newNode = (Node *) malloc(sizeof(Node));
-        newNode->data = value;
-        newNode->left = tree(nL);
-        newNode->right = tree(nR);
-    }
-    return newNode;
+GraphNode popQ(Queue *q) {
+    if (q->size < 1) return NULL;
+
+    GraphNode *current = q->head;
+    GraphNode *result = q->head;
+
+    q->head = q->head->next;
+
+    if (q->size-- > 1)
+        q->head->prev = NULL;
+    else
+        q->tail = NULL;
+
+    free(current);
+    return *result;
 }
 
-// Копипаст с лекции
-void preOrderTravers(Node *root) {
-    if (root) {
-        printf("%d ", root->data);
-        preOrderTravers(root->left);
-        preOrderTravers(root->right);
+void pushS(Stack *stack, T value) {
+    GraphNode *tmp = (GraphNode *) malloc(sizeof(GraphNode));
+    if (tmp == NULL) return;
+
+    tmp->data = value;
+    tmp->prev = stack->head;
+
+    stack->head = tmp;
+    stack->size++;
+}
+
+GraphNode popS(Stack *stack) {
+    if (stack->size < 1) return NULL;
+
+    GraphNode *current = stack->head;
+
+    stack->head = current->prev;
+    stack->size--;
+
+    free(current);
+    return *current;
+}
+
+void depthTravers(GraphNode *node) {
+    /*
+     * Помечаем как пройденный
+     */
+    node->used = true;
+    /*
+     * Для каджого дочернего связанного узла вызываем рекурсивно
+     * и убираем дочерний узел из стека дочерних
+     */
+    while (node->children){
+        GraphNode child = popS(node->children);
+        depthTravers(&child);
     }
 }
 
-void inOrderTravers(Node *root) {
-    if (root) {
-        inOrderTravers(root->left);
-        printf("%d ", root->data);
-        inOrderTravers(root->right);
+void widthTravers(GraphNode *start, Queue *q) {
+    /*
+     * Добавляем начальный узел в очередь и помечаем его как использованный
+     */
+    pushQ(q, start);
+    start->used = true;
+    /*
+     * Пока в очереди есть узлы выполняем обход
+     */
+    while (q->size) {
+        /*
+         * Достаем узел из очереди
+         */
+        GraphNode current = popQ(q);
+        /*
+         * Каждый дочерний элемент полученного узла достаём из
+         * списка дочерних элементов узла и если он не был ранее отправлен в очередь -
+         * кладем его в очередь для дальнейшей обработки, помечая как пройденный
+         */
+        while (current.children) {
+            GraphNode child = popS(current.children);
+            if (child.used == false) {
+                pushQ(q, &child);
+                child.used = true;
+            }
+        }
     }
 }
 
-void postOrderTravers(Node *root) {
-    if (root) {
-        postOrderTravers(root->left);
-        postOrderTravers(root->right);
-        printf("%d ", root->data);
+void readAndPrint(const char *path) {
+    FILE *file = fopen(path, "r");
+
+    int size;
+    fscanf(file, "%d", &size);
+
+    T matrix[size][size];
+    T res;
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            fscanf(file, "%d", &res);
+            matrix[i][j] = res;
+        }
     }
-}
 
-Node *find(Node *root, T value) {
-    if (!root)
-        return NULL;
-
-    if (root->data == value)
-        return root;
-    else if (value > root->data)
-        return find(root->right, value);
-    else if (value < root->data)
-        return find(root->left, value);
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(void) {
-    char *strHello = "Hello";
-    char *strSf = "San Francisco";
-
-    printf("Hello: %d\n", simpleHash(strHello, 5));
-    printf("San Francisco: %d\n", simpleHash(strSf, 13));
+    readAndPrint("C:\\MyData\\CLionProjects\\Geekbrains_Algorithms\\data.txt");
 }
